eba2768de9c7f1b8c987f0b08fa367b52eb41d90
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-08-08 09:31:11 +0800

    Final Submission, Lab: Exploring Lambdas and Predicate

--- src/main/java/org/example/LambdaLab.java
+++ src/main/java/org/example/LambdaLab.java
@@ -13,20 +13,20 @@ public class LambdaLab {
         // System.out.println("Is '" + str1 + "' long? " + isLong.test(str1));
         // System.out.println("Is '" + str2 + "' long? " + isLong.test(str2));
 
-        List<String> callSigns = new ArrayList<>();
-        callSigns.add("Alpha");
-        callSigns.add("Bravo");
-        callSigns.add("Archngel");
-        callSigns.add("Echo");
-        callSigns.add("Avenger");
-        Predicate<String> startsWithA = s -> s.startsWith("A");
+        // List<String> callSigns = new ArrayList<>();
+        // callSigns.add("Alpha");
+        // callSigns.add("Bravo");
+        // callSigns.add("Archngel");
+        // callSigns.add("Echo");
+        // callSigns.add("Avenger");
+        // Predicate<String> startsWithA = s -> s.startsWith("A");
         // filterAndPrint(callSigns, startsWithA, "Call signs starting with 'A'");
         
-        Predicate<String> hasLengthGreaterThan5 = s -> s.length() > 5;
-        Predicate<String> complexCondition = startsWithA.and(hasLengthGreaterThan5);
-        filterAndPrint(callSigns, complexCondition, "Starts with 'A' AND length > 5");
-        Predicate<String> doesNotStartWithA = startsWithA.negate();
-        filterAndPrint(callSigns, doesNotStartWithA, "Does NOT start with 'A'");
+        // Predicate<String> hasLengthGreaterThan5 = s -> s.length() > 5;
+        // Predicate<String> complexCondition = startsWithA.and(hasLengthGreaterThan5);
+        // filterAndPrint(callSigns, complexCondition, "Starts with 'A' AND length > 5");
+        // Predicate<String> doesNotStartWithA = startsWithA.negate();
+        // filterAndPrint(callSigns, doesNotStartWithA, "Does NOT start with 'A'");
     }
 
     public static void filterAndPrint(List<String> list, Predicate<String> predicate, String description) {
