334cb0aa8aec62bb0f19072cb3d9035ae797e73f
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-08-08 09:27:17 +0800

    feat: complete exercise 2

--- src/main/java/org/example/LambdaLab.java
+++ src/main/java/org/example/LambdaLab.java
@@ -1,14 +1,35 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class LambdaLab {
     public static void main(String[] args) {
 
-        Predicate<String> isLong = s -> s.length() > 10;
-        String str1 = "short";
-        String str2 = "This is a very long string";
-        System.out.println("Is '" + str1 + "' long? " + isLong.test(str1));
-        System.out.println("Is '" + str2 + "' long? " + isLong.test(str2));
+        // Predicate<String> isLong = s -> s.length() > 10;
+        // String str1 = "short";
+        // String str2 = "This is a very long string";
+        // System.out.println("Is '" + str1 + "' long? " + isLong.test(str1));
+        // System.out.println("Is '" + str2 + "' long? " + isLong.test(str2));
+
+        List<String> callSigns = new ArrayList<>();
+        callSigns.add("Alpha");
+        callSigns.add("Bravo");
+        callSigns.add("Archngel");
+        callSigns.add("Echo");
+        callSigns.add("Avenger");
+        Predicate<String> startsWithA = s -> s.startsWith("A");
+        filterAndPrint(callSigns, startsWithA, "Call signs starting with 'A'");
+    }
+
+    public static void filterAndPrint(List<String> list, Predicate<String> predicate, String description) {
+        System.out.println("--- " + description + " ---");
+        for (String item : list) {
+            if (predicate.test(item)) {
+                System.out.println(item);
+            }
+        }
+        System.out.println(); // Add a blank line for readability
     }
 }
